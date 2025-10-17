32542f11937e11759eae01d4faaf824d64bcf602
Jorge Rubio <jrubc16@gmail.com>
2025-10-17 14:05:11 -0600

    Add movie calification

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -1,9 +1,11 @@
 import 'package:intl/intl.dart';
 
 class HumanFormats {
-  static String number(double number) {
-    final formattedNumber = NumberFormat.compact(
-      locale: 'en_US',
+  static String number(double number, [int decimals = 0]) {
+    final formattedNumber = NumberFormat.compactCurrency(
+      decimalDigits: decimals,
+      symbol: '',
+      locale: 'en'
     ).format(number);
     return formattedNumber;
   }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delgate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -23,14 +25,18 @@ class CustomAppbar extends ConsumerWidget {
               Text('Cinemapedia', style: titleStyle),
               Spacer(),
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   final movieRepository = ref.read(movieRepositoryProvider);
-                  showSearch(
+
+                  showSearch<Movie?>(
                     context: context,
                     delegate: SearchMovieDelegate(
                       searchMovies: movieRepository.searchMovies
                     ),
-                  );
+                  ).then((movie){
+                    if(movie == null) return ;
+                    context.push('/movie/${movie.id}');
+                  });
                 },
                 icon: const Icon(Icons.search)
               ),
